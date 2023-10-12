37d0ddc93168ffc935c4a1459bc60322a1b99a27
kyleblocksom <73256380+kyleblocksom@users.noreply.github.com>
2023-10-12 10:53:26 -0700

    Initial commit

--- clean-package.py
+++ clean-package.py
@@ -1,135 +1,61 @@
-import os
-import sys
-import time
+from boto3.dynamodb.types import TypeSerializer
+from langchain.memory.chat_message_histories import DynamoDBChatMessageHistory
+from langchain.memory import ConversationBufferMemory
+
+from datetime import datetime
+import json
 import boto3
-import requests
+import os
 
-# -- Environment Variables --
+now = datetime.utcnow()
+dynamodb = boto3.client('dynamodb')
+ts = TypeSerializer()
 
-# Local Assets
-unique_package_file_name = os.environ.get("UNIQUE_PACKAGE_FILE_NAME")
-public_package_file_name = os.environ.get("EXTERNAL_PACKAGE_FILE_NAME")
-public_package__name = os.environ.get("EXTERNAL_PACKAGE_NAME")
-asset_sha256 = os.environ.get("ASSET_SHA256")
+# Create reference to DynamoDB tables
+chat_index_table_name = os.environ.get('CONVERSATION_INDEX_TABLE')
+conversation_table_name = os.environ.get('CONVERSATION_TABLE')
 
-# Private Internal Package Repository
-codeartifact_domain = os.environ.get("ExampleDomain")
-codeartifact_repo = os.environ.get("InternalRepository")
+class Chat():
 
-# Pipeline Exit and Notification
-codebuild_id = os.environ.get("CODEBUILD_BUILD_ID")
-sns_topic_arn = os.environ.get("SNSTopic")
+    def __init__(self, event):
+        print("Initializing Chat with FSI Agent")
+        self.set_user_id(event)
+        self.set_chat_index()
+        self.set_memory()
 
-def main():
-    try:
-        print("Initiating Security Scan for External Package Repository: " + public_package__name)
 
-        # Instantiate boto3 clients
-        codeguru_security_client = boto3.client('codeguru-security')
-        codeartifact_client = boto3.client('codeartifact')
-        sns_client = boto3.client('sns')
-        codebuild_client = boto3.client('codebuild')
+    def set_memory(self):
+        print("Setting Chat History")
+        _id = self.user_id + "-" + str(self.chat_index)
+        self.message_history = DynamoDBChatMessageHistory(table_name=conversation_table_name, session_id=_id)
+        self.memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=self.message_history, input_key="input", output_key="output", return_messages=True)
 
-        print("Creating CodeGuru Security Upload URL...")
 
-        create_url_input = {"scanName": public_package__name}
-        create_url_response = codeguru_security_client.create_upload_url(**create_url_input)
-        url = create_url_response["s3Url"]
-        artifact_id = create_url_response["codeArtifactId"]
+    def get_chat_index(self):
+        key = {'id':self.user_id}
+        chat_index = dynamodb.get_item(TableName=chat_index_table_name, Key=ts.serialize(key)['M'])
+        if 'Item' in chat_index:
+            return int(chat_index['Item']['chat_index']['N'])
+        return 0
 
-        print("Uploading External Package Repository File...")
 
-        upload_response = requests.put(
-            url,
-            headers=create_url_response["requestHeaders"],
-            data=open(public_package_file_name, "rb"),
-        )
+    def increment_chat_index(self):
+        self.chat_index += 1
+        input = {
+            'id': self.user_id,
+            'chat_index': self.chat_index,
+            'updated_at': str(now)
+        }
+        dynamodb.put_item(TableName=chat_index_table_name, Item=ts.serialize(input)['M'])
 
-        if upload_response.status_code == 200:
-            
-            print("Performing CodeGuru Security and Quality Scans...")
-            
-            scan_input = {
-                "resourceId": {
-                    "codeArtifactId": artifact_id,
-                },
-                "scanName": public_package__name,
-                "scanType": "Standard", # Express
-                "analysisType": "Security" # All
-            }
-            create_scan_response = codeguru_security_client.create_scan(**scan_input)
-            run_id = create_scan_response["runId"]
 
-            print("Retrieving Scan Results...")
-            
-            get_scan_input = {
-                "scanName": public_package__name,
-                "runId": run_id,
-            }
+    def create_new_chat(self):
+        self.increment_chat_index()
 
-            print("Analyzing Security and Quality Finding Severities...")
 
-            get_findings_input = {
-                "scanName": public_package__name,
-                "maxResults": 20,
-                "status": "Open",
-            }
+    def set_user_id(self, event):
+        self.user_id = "Example User"
 
-            continue_scan = True
-            while continue_scan:
-                get_scan_response = codeguru_security_client.get_scan(**get_scan_input)
 
-                if get_scan_response["scanState"] == "InProgress":                
-                    get_findings_response = codeguru_security_client.get_findings(**get_findings_input)
-
-                    if "findings" in get_findings_response:
-                        for finding in get_findings_response["findings"]:
-                            if finding["severity"] != "Low" or finding["severity"] != "Info":
-                                print("!== Amazon CodeGuru Security: Medium or High severities found. An email has been sent to the requestor with additional details. ==!")
-
-                                subject = public_package__name + " Medium to High Severy Findings"
-                                message = "Please refer to Amazon CodeGuru Security scan: " + str(public_package__name)
-                                sns_client.publish(
-                                    TopicArn=sns_topic_arn,
-                                    Subject=subject,
-                                    Message=message,
-                                )
-                                continue_scan = False
-                                sys.exit()
-                else:
-                    break
-
-            print("Publishing InfoSec Validated Package Repository to Private Internal CodeArtifact...")
-            source_path = os.getcwd() + "/" + unique_package_file_name
-
-            with open(source_path, 'rb') as asset_content:
-                file_bytes = asset_content.read()
-
-            codeartifact_response = codeartifact_client.publish_package_version(
-                domain=codeartifact_domain,
-                repository=codeartifact_repo,
-                format='generic',
-                namespace=public_package__name,
-                package=public_package__name,
-                packageVersion='Latest',
-                assetContent=file_bytes,
-                assetName=unique_package_file_name,
-                assetSHA256=asset_sha256,
-                unfinished=True
-            )
-
-            print("CodeArtifact response = " + str(codeartifact_response))
-            subject = "InfoSec Approved " + public_package__name
-            message = "Please refer to Amazon CodeArtifact private package: " + str(public_package__name)
-            sns_client.publish(
-                TopicArn=sns_topic_arn,
-                Subject=subject,
-                Message=message,
-            )
-        else:
-            raise Exception(f"Source failed to upload external package to CodeGuru Security with status {upload_response.status_code}")
-    except Exception as error:
-        print(f"Action Failed, reason: {error}")
-
-if __name__ == "__main__":
-    main()
+    def set_chat_index(self):
+        self.chat_index = self.get_chat_index()
